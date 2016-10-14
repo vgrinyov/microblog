d93a66612b42c366130852f9ac00b3ed562b1863
vgrinyov <vitaliy.grinyov@gmail.com>
2016-10-15 00:43:41 +0300

    refactor routes and add their to the pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,51 +1,50 @@
 require 'rails_helper'
 
 describe "StaticPages" do
-  let(:base_title) {"Microblog"}
   describe "Home page" do
     it "should  have the content 'Microblog'" do
-      visit 'static_pages/home'
+      visit root_path
       expect(page).to have_content('Microblog')
     end
     it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title}")
+      visit root_path
+      expect(page).to have_title("Microblog")
     end
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('| Home')
     end
   end
 
   describe "Help page" do
     it "should  have the content 'Help'" do
-      visit 'static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
+      visit help_path
+      expect(page).to have_title("Microblog | Help")
     end
   end
 
   describe "About page" do
     it "should  have the content 'About Us'" do
-      visit 'static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About Us")
+      visit about_path
+      expect(page).to have_title("Microblog | About Us")
     end
   end
   describe "Contact" do
     it "should  have the content 'Contact'" do
-      visit 'static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} | Contact")
+      visit contact_path
+      expect(page).to have_title("Microblog | Contact")
     end
   end
 end
