55a76ec02ef6739b034f8d7b2ae6b87067d10716
vgrinyov <vitaliy.grinyov@gmail.com>
2016-10-15 11:28:39 +0300

    correct root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#home'
-  match '/help', to: 'static_pages#help', via: 'get'
-  match '/about', to: 'static_pages#about', via: 'get'
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
