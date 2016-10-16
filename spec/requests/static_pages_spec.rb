e6605a4632905e95cd1a898881257668b3da6e10
vgrinyov <vitaliy.grinyov@gmail.com>
2016-10-16 11:14:18 +0300

    add integration test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,26 +6,26 @@ describe "StaticPages" do
   subject { page }
 
   describe "Home page" do
-    before {visit root_path}
-    it { should  have_content('Microblog') }
-    it { should  have_title(full_title('')) }
+    before { visit root_path }
+    it { should have_content('Microblog') }
+    it { should have_title(full_title('')) }
     it { should_not have_content('| Home') }
   end
 
   describe "Help page" do
-    before {visit help_path}
-    it { should  have_content('Help') }
-    it { should  have_title(full_title('Help')) }
+    before { visit help_path }
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
-    before {visit about_path}
-    it { should  have_content('About') }
-    it { should  have_title(full_title('About Us')) }
+    before { visit about_path }
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
   end
   describe "Contact" do
-    before {visit contact_path}
-    it { should  have_content('Contact') }
-    it { should  have_title(full_title('Contact')) }
+    before { visit contact_path }
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
