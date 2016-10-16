765004dec6d93b4c93d79e61d79860e74585f914
vgrinyov <vitaliy.grinyov@gmail.com>
2016-10-16 14:40:23 +0300

    add route and view for signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   get 'users/new'
 
-  root  'static_pages#home'
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
+  root 'static_pages#home'
+  match '/signup', to: 'static_pages#new', via: 'get'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
