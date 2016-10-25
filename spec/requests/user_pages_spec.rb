cdc9fa58808ae7a9b348fb0138f77ac66133a518
vgrinyov <vitaliy.grinyov@gmail.com>
2016-10-25 17:44:43 +0300

    add test for sign up page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,7 +13,7 @@ describe "User pages" do
     end
   describe "profile page" do
 
-    #replace
+    let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
