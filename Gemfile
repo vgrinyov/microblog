443203ebb6783e075db622d6df4c126a5182096b
vgrinyov <vitaliy.grinyov@gmail.com>
2016-10-25 17:33:55 +0300

    add resource user

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ end
 group :test do
   gem 'selenium-webdriver'
   gem 'capybara'
+  gem 'factory_girl_rails'
 end
 
 group :doc do
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,6 +10,15 @@ describe "User pages" do
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
 
+    end
+  describe "profile page" do
+
+    #replace
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+
   end
 
 end
